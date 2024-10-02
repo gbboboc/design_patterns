d6b9196acee49742cefb8f81e5d62a548e194bd1
DeGambino <126100679+DeGambino@users.noreply.github.com>
2024-10-02 23:25:14 +0300

    Add Static Import + Edit Singleton pattern

---
+++ StaticImport/greetings.js
@@ -0,0 +1,12 @@
+// greetings.js
+export function sayMorning() {
+	return "Good morning!";
+}
+
+export function sayAfternoon() {
+	return "Good afternoon!";
+}
+
+export function sayEvening() {
+	return "Good evening!";
+}
